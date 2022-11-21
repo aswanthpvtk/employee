ffe744095c51d2df8a4bcafb2af631529a2c7b98
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-21 15:20:03 +0530

    emp

---
+++ employee.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class employee {
+
+    public static void main(String[] args) {
+        int op;
+        System.out.println("-----------------------------------");
+        System.out.println("enter your option");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1.Add\n2.View\n4.Search\n4.Delete\n5.Exit");
+        op = sc.nextInt();
+
+    }
+}
